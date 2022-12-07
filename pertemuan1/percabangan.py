d2f083e9e376dc4fbd1f982dee4a0d20e002d5f0
DheaMarsella <114457865+DheaMarsella@users.noreply.github.com>
2022-12-07 18:20:02 +0700

    Add files via upload

---
+++ pertemuan1/percabangan.py
@@ -0,0 +1,8 @@
+num = eval(input("Masukkan Integer: "))
+
+if num > 0:
+    print("Bilangan Positif")
+elif num == 0:
+    print("Bilangan nol")
+else:
+    print("Bilangan Negatif")
---
+++ pertemuan1/variable.py
@@ -0,0 +1,8 @@
+namaLengkap = "Dhea Marsella" # string
+asal = "Depok" # string
+Umur = 20 # Integer
+Hobby = "Baca" # string
+isStudent = True # boolean
+
+print('Halo nama saya', namaLengkap)
+
